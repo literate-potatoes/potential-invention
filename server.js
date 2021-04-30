3905a9a38f08792f3e62a670a123a80ebd1c7a03
Kamal Nasser <kamal@digitalocean.com>
2021-04-30 21:45:34 +0300

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const host = '0.0.0.0';
+const port = 8080;
+
+const requestListener = function (req, res) {
+	res.end('yeet');
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+	    console.log(`Server is running on http://${host}:${port}`);
+});
